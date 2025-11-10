1f8c01d1339220c4211b7d5e622d5309ec725656
Nibesh Shrestha <nibesh.mastran@gmail.com>
2025-11-10 15:01:54 -0500

    change package name to multi_sender_beb

--- node/src/main.rs
+++ node/src/main.rs
@@ -6,7 +6,7 @@ use config::Import as _;
 use config::{Comm, Committee, KeyPair, Parameters};
 use crypto::SignatureService;
 use env_logger::Env;
-use single_sender_beb::{Block, Consensus};
+use multi_sender_beb::{Block, Consensus};
 use store::Store;
 use tokio::sync::mpsc::{channel, Receiver};
 
